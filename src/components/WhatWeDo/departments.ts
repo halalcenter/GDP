e747f0d03be1b0fb9a19483083ec3c7c3cb0dd8c
halalcenter <gdp.rgpv@gmail.com>
2025-01-26 14:57:23 +0530

    Update departments.ts

--- src/components/WhatWeDo/departments.ts
+++ src/components/WhatWeDo/departments.ts
@@ -22,11 +22,11 @@ export const departments: Department[] = [
   },
   {
     title: "CRESCENDO",
-    description: "Crescendo is for those passionate about the auditory arts. Whether it's composing original tracks, producing sound for events, or managing music-related activities, this department works to bring energy and creativity through sound. Members of the Music department also collaborate with other teams to enhance the overall experience of events with live performances, background scores, and thematic soundtr"
+    description: "Crescendo is for those passionate about the auditory arts. Whether it's composing original tracks, producing sound for events, or managing music-related activities, this department works to bring energy and creativity through sound. Members of the Music department also collaborate with other teams to enhance the overall experience of events with live performances, background scores, and thematic soundtracks"
   },
   {
     title: "CAMPUS HANGOUTS",
-    description: "Hangouts is special interactive session specifically targeting the campus students. The session is like an informal group discussion between campus junta and pioneers of their respective fields. It involves discussions and brainstorming on technical industrial, economical, and many other matters. Throughout the year multiple hangout sessions will be orga"
+    description: "Hangouts is special interactive session specifically targeting the campus students. The session is like an informal group discussion between campus junta and pioneers of their respective fields. It involves discussions and brainstorming on technical industrial, economical, and many other matters. Throughout the year multiple hangout sessions will be organized"
   }
 
 
